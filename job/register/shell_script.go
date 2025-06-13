e77757cace74ec3f55d350c46edde7cf03c58cbd
BorONE <bor1.ss@yandex.ru>
2025-06-13 20:20:32 +0300

    init

---
+++ job/register/shell_script.go
@@ -0,0 +1,33 @@
+package register
+
+import (
+	"context"
+	"os/exec"
+	"pipegraph/config"
+	"pipegraph/job"
+)
+
+type ShellScriptJob struct {
+	cmd    *exec.Cmd
+	ctx    context.Context
+	cancel func()
+}
+
+func (j *ShellScriptJob) Init(job *config.Job) {
+	j.ctx, j.cancel = context.WithCancel(context.Background())
+	j.cmd = exec.CommandContext(j.ctx, "/bin/sh", *job.Path)
+}
+
+func (j *ShellScriptJob) Run() error {
+	return j.cmd.Run()
+}
+
+func (j *ShellScriptJob) Stop() {
+	j.cancel()
+}
+
+var _ job.Job = &ShellScriptJob{}
+
+func init() {
+	job.RegisterJobType(config.JobType_ShellScript, func() job.Job { return &ShellScriptJob{} })
+}
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"pipegraph/config"
+	"pipegraph/graph"
+
+	_ "pipegraph/job/register"
+
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+func main() {
+	file, err := os.Open("graph.proto.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	graphConfig := &config.Graph{}
+	err = prototext.Unmarshal(fileData, graphConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(prototext.Format(graphConfig))
+
+	gr := graph.NewGraph(graphConfig)
+	go func() {
+		for {
+			gr.GetNode(2).Reset()
+		}
+	}()
+	gr.Run()
+
+}
