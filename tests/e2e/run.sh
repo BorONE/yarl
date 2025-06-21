f7b13d00b360f7d40a2717f8f43201fc38d00fa1
BorONE <bor1.ss@yandex.ru>
2025-06-21 19:43:49 +0300

    add e2e test

---
+++ tests/e2e/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+function rm_ts {
+    date_re='[0-9]*/[0-9]*/[0-9]*'
+    time_re='[0-9]*:[0-9]*:[0-9]*'
+    sed "s#$date_re $time_re ##"
+}
+
+function client {
+    cmd="go run client/main.go --cmd $@"
+    echo "$ ${cmd}" >> $output/client
+
+    $cmd 2>&1 | rm_ts >> $output/client
+}
+
+output=$(dirname $0)/output
+canon=$(dirname $0)/canon
+rm -rf $output
+mkdir -p $output
+
+go run main.go 2>&1 | rm_ts > $output/server &
+while ! lsof -i :9000 &> /dev/null; do
+    true
+done
+pid=$(lsof -i :9000 | tail -1 | awk '{print $2}')
+trap 'kill $pid' EXIT
+
+> $output/client
+client config
+client state
+client run
+client state
+
+if [[ $1 == canonize ]]; then
+    rm -rf $canon
+    cp -r $output $canon
+    rm -rf $output
+else
+    diff $output $canon && rm -rf $output
+fi
